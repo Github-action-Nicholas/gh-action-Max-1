4e93edb3d4e490c1fbb3d66f5adb0687294e09a8
Nick Zhou <smz1680@gmail.com>
2023-10-03 22:34:12 -0500

    fixed test failing!

--- src/components/HelpBox.jsx
+++ src/components/HelpBox.jsx
@@ -4,8 +4,8 @@ import "./HelpBox.css";
 
 function HelpBox({ title, text }) {
   return (
-    <article className="help-box">
-      <h3>{title}""</h3>
+    <article className="help-boxs">
+      <h3>{title}</h3>
       <p>{text}</p>
     </article>
   );
